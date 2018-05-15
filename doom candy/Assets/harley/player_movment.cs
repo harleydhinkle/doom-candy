a9ae95295d7dd567b868a07210eda634d4ab626f
Unknown <wolfking068@gmail.com>
2018-05-14 17:12:21 -0700

    movemant

---
+++ doom candy/Assets/harley/player_movment.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_movment : MonoBehaviour {
+    public float speed;
+    public controller controller;
+    Vector3 desierv;
+    float horizontal;
+    float vertical;
+    public Rigidbody rm;
+    public Vector3 curlook;
+    // Use this for initialization
+    void Start () {
+        rm = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        horizontal = controller.horizontal;
+        Vector3 myright = new Vector3(horizontal, 0, 0);
+        vertical = controller.vertical;
+        Vector3 myup = new Vector3(0, 0, vertical);
+        desierv = (myup + myright) * speed * Time.deltaTime;
+        rm.velocity = desierv;
+
+    }
+}
