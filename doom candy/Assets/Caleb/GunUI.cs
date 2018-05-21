7047e99d18200aeec2ba7a28862540d737fbd4c6
Unknown <s179067@students.aie.edu.au>
2018-05-21 15:11:21 -0700

    Auto stash before merge of "calub" and "origin/calub"

---
+++ doom candy/Assets/Caleb/GunUI.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GunUI : MonoBehaviour {
+
+    public Text health;
+    public Text armor;
+    public Text weapon;
+    public Text ammo;
+    public Text score;
+    public Text time;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //health.text = ;
+        //armor.text = ;
+        //weapon.text = ;
+        //ammo.text = ;
+        //score.text = ;
+        //time.text = ;
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //health.text = ;
+        //armor.text = ;
+        //weapon.text = ;
+        //ammo.text = ;
+        //score.text = ;
+        //time.text = ;
+
+        //if ()
+        //{
+
+        //}
+        //else if()
+        //{
+                                  
+        //}
+
+    }
+}
