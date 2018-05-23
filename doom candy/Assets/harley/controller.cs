3f1c31293d0f13b9cba6b37c9c705c3f7f306f94
Unknown <wolfking068@gmail.com>
2018-05-23 12:57:01 -0700

    stuff

--- doom candy/Assets/harley/controller.cs
+++ doom candy/Assets/harley/controller.cs
@@ -22,8 +22,8 @@ public class controller : MonoBehaviour {
     public bool reloadgun;
     IEnumerator startviprat()
     {
-        GamePad.SetVibration(playerIndex, state.Triggers.Right, state.Triggers.Right);
-        yield return new WaitForSeconds(.2f);
+        GamePad.SetVibration(playerIndex, leftrun, rightrun);
+        yield return new WaitForSeconds(.4f);
         GamePad.SetVibration(playerIndex, 0, 0);
     }
     // Use this for initialization
