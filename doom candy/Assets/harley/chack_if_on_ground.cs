47704742b07fa76b61f2affceddfeae27f7604f5
Unknown <wolfking068@gmail.com>
2018-06-04 16:48:30 -0700

    yey

---
+++ doom candy/Assets/harley/chack_if_on_ground.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class chack_if_on_ground : MonoBehaviour {
+    public bool isGrounded;
+    public float hitdistance;
+    public LayerMask layer;
+    public float down;
+    public player_movment player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateStates();
+	}
+    public void UpdateStates()
+    {
+        if(isGrounded == true)
+        {
+            hitdistance = 0.35f;
+        }
+        else
+        {
+            hitdistance = 0.15f;
+            player.rm.AddForce(Vector3.down * down);
+
+        }
+        if (Physics.Raycast(transform.position, -transform.up, hitdistance, layer))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+}
