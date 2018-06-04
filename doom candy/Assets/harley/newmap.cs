4e55237e80556aa422485f811ef1491e30277251
Unknown <wolfking068@gmail.com>
2018-06-04 11:44:15 -0700

    stuff

---
+++ doom candy/Assets/harley/newmap.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class newmap : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter(Collision hit)
+    {
+        if (hit.gameObject.tag == "Player1")
+        {
+            SceneManager.LoadScene("");
+        }
+
+
+    }
+}
---
+++ doom candy/Assets/harley/shooting ai.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootingai : MonoBehaviour {
+    public Rigidbody fire;
+    public float throwpower;
+    public float radius;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void kill()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius);
+        foreach (Collider hit in hitColliders)
+        {
+            if (hit.tag == "Player1")
+            {
+                Rigidbody fire2 = Instantiate(fire, transform.position, transform.rotation) as Rigidbody;
+                fire2.velocity = transform.position * throwpower;
+            }
+
+
+
+        }
+    }
+}
