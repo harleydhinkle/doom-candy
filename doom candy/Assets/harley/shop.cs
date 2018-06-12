e2a38bfa3e333d65af4a30921475d013b2fd2a89
Unknown <wolfking068@gmail.com>
2018-06-12 11:08:49 -0700

    shop

--- doom candy/Assets/harley/shop.cs
+++ doom candy/Assets/harley/shop.cs
@@ -20,6 +20,9 @@ public class shop : MonoBehaviour
     public int change2;
     public int change3;
     public int change4;
+    public bool gundamigeprice;
+    public bool gunranigeprice;
+    public bool batprice;
     public void damige_for_gun()
     {
         if (gundamigepriceprice <= player.points && gun.damige < maxdamige)
@@ -29,6 +32,12 @@ public class shop : MonoBehaviour
             player.points = change2;
             player.play.score2();
             gun.damige = Mathf.Clamp(gun.damige, 0, maxdamige);
+            gundamigeprice = true;
+            if(gundamigeprice == true)
+            {
+                gundamigepriceprice += 5;
+                gundamigeprice = false;
+            }
         }
     }
     public void range_for_gun()
@@ -40,6 +49,12 @@ public class shop : MonoBehaviour
             change3 = player.points -= rangeforgunprice;
             player.points = change3;
             player.play.score2();
+            gunranigeprice = true;
+            if (gunranigeprice == true)
+            {
+                rangeforgunprice += 5;
+                gunranigeprice = false;
+            }
         }
     }
     public void damige_for_bashballbat()
@@ -51,6 +66,12 @@ public class shop : MonoBehaviour
             change4 = player.points -= damegeforbatprice;
             player.points = change4;
             player.play.score2();
+            batprice = true;
+            if(batprice == true)
+            {
+                damegeforbatprice += 5;
+                batprice = false;
+            }
         }
     }
 }
