11d9cd96e61851c294faedf9d538aac4eadd750b
Unknown <s179067@students.aie.edu.au>
2018-06-18 09:54:21 -0700

    UI updates and player additions
    UI now has squish with buttons and in game canvas has been updated

---
+++ Assets/Caleb/Scripts/PlayerFaceUI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum FaceStates
+{
+    damaged,
+    normal,
+    shoot,
+    get
+}
+
+public class PlayerFaceUI : MonoBehaviour {
+    //create an array
+    public Sprite[] faceUI;
+    //for the image
+    public Image face;
+    //for testing
+    public FaceStates testFace;
+
+
+    //called in order to change face
+    void changeFace(FaceStates state)
+    {
+        //if statement to prevent errors
+        if((int)state >= faceUI.Length) { return; }
+        face.sprite = faceUI[(int)state];
+
+    }
+    //allows for public use of changing faces
+    public void playfaceeffect(FaceStates face)
+    {
+        StartCoroutine(faces(face));
+    }
+    //changes faces for action and returns to normal
+    IEnumerator faces(FaceStates face)
+    {
+        changeFace(face);
+        yield return new WaitForSeconds(.4f);
+        changeFace(FaceStates.normal);
+    }
+}
